d9119093a2059370b7077a2e30ed0b8d451feeac
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-04-10 12:09:38 +0300

    Right tags and right flow of locale selector.

--- 0x02-i18n/app.py
+++ 0x02-i18n/app.py
@@ -20,18 +20,18 @@ class Config:
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"}
+    }
 
 
 def get_user():
     """
     Returns a dictionary depending on the id passed to login_as parameter.
     """
-    users = {
-        1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-        2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-        3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-        4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"}
-        }
     user_id = request.args.get('login_as')
     if user_id:
         user_id = int(user_id)
@@ -53,8 +53,8 @@ def get_locale():
     """
     lang = request.args.get('locale')
     user_locale = None
-    if lang == 'fr':
-        return 'fr'
+    if lang in app.config["LANGUAGES"]:
+        return lang
     if g.user:
         user_locale = g.user.get('locale')
     if user_locale in app.config['LANGUAGES']:

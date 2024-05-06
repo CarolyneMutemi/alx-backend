a1e692293f3bf1b4bfbe3cf4ab9fb081fafceec4
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-05-07 02:54:13 +0300

    Node Redis Client

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-console */
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => console.log('Redis Client Error', err));
+client.on('connect', () => { console.log('Redis client connected to the server'); });
